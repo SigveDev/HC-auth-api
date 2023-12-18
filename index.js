cef46af9609e107835cd4399a037f8eb2e55a6ad
2derp4you <iam2derp4you@gmail.com>
2023-12-18 19:56:43 +0100

    Update console log message for server start

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.get('/pfp', (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('Server started on port ' + process.env.PORT);
+    console.log('Using port: ' + process.env.PORT);
 });
 
 module.exports = app;
